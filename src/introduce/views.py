351ab848dd0ffaf733ab68fd1163d29a37dfe28c
recodeyo1ko <kuronekoryohei@gmail.com>
2024-07-10 21:36:35 +0900

    基本ページのひな形、ページの到達確認

--- src/introduce/views.py
+++ src/introduce/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def introduce(request):
+    return render(request, 'introduce/introduce.html')
+
+def index(request):
+    return render(request, 'introduce/index.html')
+
+def form(request):
+    return render(request, 'introduce/form.html')
